d03071f7b618bdbc4c9d9e99c9ebf206dba19c83
Prince Pereira <ppereira@microsoft.com>
2026-01-02 16:05:40 +0530

    New changes.

--- modules/Delete-Cluster.ps1
+++ modules/Delete-Cluster.ps1
@@ -7,3 +7,5 @@ $clusterName = $Global:CLUSTER_NAME
 Write-Host "Deleting AKS Cluster: $clusterName in Resource Group: $rgName" -ForegroundColor Cyan
 az aks delete --resource-group $rgName --name $clusterName --yes
 Write-Host "AKS Cluster: $clusterName deleted successfully." -ForegroundColor Green
+kubectl config delete-context $clusterName
+Write-Host "Kubernetes context for Cluster: $clusterName deleted successfully." -ForegroundColor Green
