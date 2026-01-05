dfbe425f4b1322d67081ba15fd9b2410bd9067c9
Prince Pereira <ppereira@microsoft.com>
2026-01-05 11:27:49 +0530

    New changes.

--- Create-PodsAndServices.ps1
+++ Create-PodsAndServices.ps1
@@ -1,5 +1,10 @@
 Write-Host "Creating Pods and Services..." -ForegroundColor Cyan
 kubectl create -f .\Yamls\hpc-ds-win22.yaml
 kubectl create -f .\Yamls\dep-test.yaml
-kubectl create -f .\Yamls\Svc-*
+kubectl create -f .\Yamls\Svc-IPV4.yaml
+kubectl create -f .\Yamls\Svc-IPV6.yaml
+kubectl create -f .\Yamls\Svc-Pref-DUAL-Cluster.yaml
+kubectl create -f .\Yamls\Svc-Pref-DUAL-Local.yaml
+kubectl create -f .\Yamls\Svc-Req-DUAL-Cluster.yaml
+kubectl create -f .\Yamls\Svc-Req-DUAL-Local.yaml
 Write-Host "Pods and Services created successfully." -ForegroundColor Green
