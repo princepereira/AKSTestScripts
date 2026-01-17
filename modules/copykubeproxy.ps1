df07cbd8e546fe2bee27b7ae4a188bcf34c79f6f
Prince Pereira <ppereira@microsoft.com>
2026-01-17 12:36:37 +0530

    New changes.

--- modules/copykubeproxy.ps1
+++ modules/copykubeproxy.ps1
@@ -1,6 +1,7 @@
-rm -r -Force kubeproxy -ErrorAction SilentlyContinue
+Write-Host "Deploying modified kube-proxy binary" -ForegroundColor Yellow
+Remove-Item -Recurse -Force kubeproxy -ErrorAction SilentlyContinue
 Expand-Archive kubeproxy.zip -DestinationPath kubeproxy -Force
-if (-not (Test-Path -Path C:\k\kube-proxy.exe)) {
+if (-not (Test-Path -Path C:\k\kube-proxy_Orig.exe)) {
     Copy-Item -Path C:\k\kube-proxy.exe -Destination C:\k\kube-proxy_Orig.exe -Force
     Copy-Item -Path .\sfpcopy.exe -Destination C:\k\sfpcopy.exe -Force
 }
@@ -9,3 +10,4 @@ Start-Sleep -Seconds 2
 rm C:\k\kubeproxy.err.log -ErrorAction SilentlyContinue
 Restart-Service -Force hns
 Start-Service KubeProxy
+Write-Host "kube-proxy deployment script completed." -ForegroundColor Green
--- modules/replacekubeproxywithoriginal.ps1
+++ modules/replacekubeproxywithoriginal.ps1
@@ -1,6 +1,8 @@
+Write-Host "Restoring original kube-proxy binary" -ForegroundColor Yellow
 c:\k\sfpcopy.exe C:\k\kube-proxy_Orig.exe C:\k\kube-proxy.exe
 Stop-Service -Force KubeProxy
 Start-Sleep -Seconds 2
 rm C:\k\kubeproxy.err.log -ErrorAction SilentlyContinue
 Restart-Service -Force hns
 Start-Service KubeProxy
+Write-Host "kube-proxy restoration script completed." -ForegroundColor Green
