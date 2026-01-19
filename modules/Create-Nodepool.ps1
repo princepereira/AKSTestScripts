92fd2a94a2256261b34896a6367696911e400e75
Prince Pereira <ppereira@microsoft.com>
2026-01-19 12:24:44 +0530

    New changes.

--- modules/Create-Nodepool.ps1
+++ modules/Create-Nodepool.ps1
@@ -9,8 +9,8 @@ $osSku = $Global:OS_SKU
 $nodeVmSize = $Global:NODE_VM_SIZE
 
 Write-Host "Creating Windows Node Pool: $nodePoolName in Cluster: $clusterName, OS SKU: $osSku, Node Count: $nodeCount" -ForegroundColor Cyan
-New-AzAksNodePool -ResourceGroupName $rgName -ClusterName $clusterName -Name $nodePoolName -VmSize Standard_D4s_v5 -Count $nodeCount -OsType 'Windows' -OsSKU $osSku -VmSetType 'VirtualMachineScaleSets' -OsDiskSize 256
-az aks nodepool add --resource-group $rgName --cluster-name $clusterName --os-type Windows --os-sku $osSku --node-vm-size standard_e8-2as_v5 --name $nodePoolName --node-count $nodeCount
+Write-Host "Executing [New-AzAksNodePool -ResourceGroupName $rgName -ClusterName $clusterName -Name $nodePoolName -VmSize $nodeVmSize -Count $nodeCount -OsType 'Windows' -OsSKU $osSku -VmSetType 'VirtualMachineScaleSets' -OsDiskSize 256]" -ForegroundColor Yellow
+New-AzAksNodePool -ResourceGroupName $rgName -ClusterName $clusterName -Name $nodePoolName -VmSize $nodeVmSize -Count $nodeCount -OsType 'Windows' -OsSKU $osSku -VmSetType 'VirtualMachineScaleSets' -OsDiskSize 256
 Write-Host "Waiting for 5 minutes for the nodes to be ready..." -ForegroundColor Yellow
 Start-Sleep -Seconds 300
 Write-Host "Windows Node Pool: $nodePoolName created successfully in Cluster: $clusterName." -ForegroundColor Green
